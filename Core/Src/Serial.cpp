868bb681b49f05a0019dcbbcc1525ed8da40eb3a
Arias-co <50190921+Arias-co@users.noreply.github.com>
2020-12-06 10:14:04 -0500

    Summary

--- Core/Src/Serial.cpp
+++ Core/Src/Serial.cpp
@@ -14,6 +14,42 @@ Serial::Serial(UART_HandleTypeDef * uartx)
     uart = uartx;
 }
 
+void Serial::receive( uint8_t * data, uint16_t timeout,
+        Mode_timeout_t modeTimeout )
+{
+    uint8_t *bufer = data;
+    uint32_t lastTime = HAL_GetTick();
+
+    *bufer = (uint8_t) uart->Instance->DR;
+    bufer++;
+
+    if ( modeTimeout == TIMEOUT )
+    {
+        while ( ( HAL_GetTick() - lastTime ) < timeout )
+        {
+            if ( __HAL_UART_GET_FLAG( uart, UART_FLAG_RXNE ) )
+            {
+                *bufer = (uint8_t) ( uart->Instance->DR & (uint8_t) 0x00FF );
+                bufer++;
+            }
+        }
+    }
+    else if(modeTimeout == INTER_BYTE_TIMEOUT)
+    {
+        if(timeout == 1) timeout++;
+
+        while ( ( HAL_GetTick() - lastTime ) < timeout )
+        {
+            if ( __HAL_UART_GET_FLAG( uart, UART_FLAG_RXNE ) )
+            {
+                *bufer = (uint8_t) uart->Instance->DR ;
+                bufer++;
+                lastTime = HAL_GetTick();
+            }
+        }
+    }
+}
+
 void Serial::write( uint8_t  *text )
 {
 
--- Core/Src/Serial.h
+++ Core/Src/Serial.h
@@ -13,6 +13,12 @@
 #include "stm32f1xx_hal.h"
 
 
+typedef enum
+{
+    TIMEOUT,
+    INTER_BYTE_TIMEOUT
+} Mode_timeout_t;
+
 class Serial: public Print
 {
 private:
@@ -21,6 +27,7 @@ public:
 
     UART_HandleTypeDef *uart;
     Serial( UART_HandleTypeDef * uartx );
+    void receive( uint8_t * data, uint16_t timeout , Mode_timeout_t modeTimeout);
     virtual void write( uint8_t * text );
 
 };
